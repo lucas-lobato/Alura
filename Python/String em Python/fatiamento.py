f8299c1a6b61824021053077e9a51273b6bbded1
lucas-lobato <lucaslobato1999@gmail.com>
2022-10-05 17:47:34 -0300

    update

--- Python/String em Python/fatiamento.py
+++ Python/String em Python/fatiamento.py
@@ -1,19 +1,57 @@
+import re
+
+
+class ExtratorURL:
+    def __init__(self, url):
+        self.url = self.sanitiza_url(url)
+        self.valida_url()
+
+    def sanitiza_url(self, url):
+        if type(url) == str:
+            return url.strip()
+        else:
+            return ""
+
+    def valida_url(self):
+        if not self.url:
+            raise ValueError("A URL está vazia")
+
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(self.url)
+        if not match:
+            raise ValueError("A URL não é válida.")
+
+    def get_url_base(self):
+        indice_interrogacao = self.url.find('?')
+        url_base = self.url[:indice_interrogacao]
+        return url_base
+
+    def get_url_parametros(self):
+        indice_interrogacao = self.url.find('?')
+        url_parametros = self.url[indice_interrogacao + 1:]
+        return url_parametros
+
+    def get_valor_parametro(self, parametro_busca):
+        indice_parametro = self.get_url_parametros().find(parametro_busca)
+        indice_valor = indice_parametro + len(parametro_busca) + 1
+        indice_e_comercial = self.get_url_parametros().find('&', indice_valor)
+        if indice_e_comercial == -1:
+            valor = self.get_url_parametros()[indice_valor:]
+        else:
+            valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
+        return valor
+
+    def __len__(self):
+        return len(self.url)
+    
+    def __str__(self):
+        return self.url + "\n" + "Parâmetros: " + self.get_url_parametros() + "\n" + "URL Base: " + self.get_url_base()
+    
+    def __eq__(self, other):
+        return self.url == other.url
+
+
 url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
-
-# Separa base e parâmetros
-indice_interrogacao = url.find('?')
-url_base = url[:indice_interrogacao]
-url_parametros = url[indice_interrogacao+1:]
-print(url_parametros)
-
-# Busca o valor de um parâmetro
-parametro_busca = 'quantidade'
-indice_parametro = url_parametros.find(parametro_busca)
-indice_valor = indice_parametro + len(parametro_busca) + 1
-indice_e_comercial = url_parametros.find('&', indice_valor)
-if indice_e_comercial == -1:
-    valor = url_parametros[indice_valor:]
-else:
-    valor = url_parametros[indice_valor:indice_e_comercial]
-
-print(valor)
+extrator_url = ExtratorURL(url)
+valor_quantidade = extrator_url.get_valor_parametro("quantidade")
+print(valor_quantidade)
